bfc49ed49ff594113fb7e56b6c2a958c4b90e793
29hashir04 <hasheralwara@gmail.com>
2024-12-28 21:05:02 +0500

    Done

--- chapter 5/asyncio_coroutine.py
+++ chapter 5/asyncio_coroutine.py
@@ -44,6 +44,23 @@ def state3(transition_value):
     time.sleep(1)
 
     print('....evaluating....')
+    if input_value == 0:
+        result = yield from state1(input_value)
+    else:
+        result = yield from end_state(input_value)
+    
+    return output_value + 'State 3 calling %s' % result
+
+def end_state(transition_value):
+    output_value = 'End State with transition value = %s\n' % transition_value
+    print('....Stop Computation....')
+
+    return output_value
+
+if __name__ == '__main__':
+    print('Finite State Machine simulatio9n with asyncio coroutine')
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start_state())
 
 
 
